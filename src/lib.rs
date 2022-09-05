e646ac9190b801fbf4646cc5b69d7e6975f8988b
KENZ <KENZ.gelsoft@gmail.com>
2022-09-06 06:51:55 +0900

    Add macOS specific impl

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,8 @@
 pub fn wx_config(args: &[&str]) -> Vec<String> {
     wx_x86_64_pc_windows_msvc::wx_config(args)
 }
+
+#[cfg(target_os = "macos")]
+pub fn wx_config(args: &[&str]) -> Vec<String> {
+    wx_universal_apple_darwin::wx_config(args)
+}
