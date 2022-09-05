c64171791fb00088f0ebd530fff675299e7518d3
KENZ <KENZ.gelsoft@gmail.com>
2022-09-06 06:13:12 +0900

    Change wx-x86_64-pc-windows-msvc to windows-specific dependency

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
+#[cfg(windows)]
 use wx_x86_64_pc_windows_msvc;
 
 pub fn wx_config(args: &[&str]) -> Vec<String> {
-    wx_x86_64_pc_windows_msvc::wx_config(args)
+    if cfg!(windows) {
+        wx_x86_64_pc_windows_msvc::wx_config(args)
+    } else {
+        Vec::new()
+    }
 }
