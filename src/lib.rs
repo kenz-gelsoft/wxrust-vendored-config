67a49d3f9501c353fa88c8e9f139a76786ada576
KENZ <KENZ.gelsoft@gmail.com>
2022-09-06 23:43:37 +0900

    Windows-MinGW specific impl

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,11 @@ pub fn wx_config(args: &[&str]) -> Vec<String> {
     wx_x86_64_pc_windows_msvc::wx_config(args)
 }
 
+#[cfg(all(windows, target_env = "gnu"))]
+pub fn wx_config(args: &[&str]) -> Vec<String> {
+    wx_x86_64_pc_windows_gnu::wx_config(args)
+}
+
 #[cfg(target_os = "macos")]
 pub fn wx_config(args: &[&str]) -> Vec<String> {
     wx_universal_apple_darwin::wx_config(args)
